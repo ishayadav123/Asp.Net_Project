22cdd5fe240cb8cffecbda4702c90f2c382922d0
ishayadav123 <68954682+ishayadav123@users.noreply.github.com>
2020-09-10 16:49:44 +0530

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppRegisLogin.DBModel;
+using WebAppRegisLogin.Models;
+
+namespace WebAppRegisLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        UserDBEntities objUserDBEntities = new UserDBEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            UserModel objUserModel = new UserModel();
+            return View(objUserModel);
+        }
+        [HttpPost]
+        public ActionResult Register(UserModel objUserModel)
+        {
+            if (ModelState.IsValid)
+            {
+                User objUser = new DBModel.User();
+                objUser.CreatedOn = DateTime.Now;
+                objUser.Email = objUserModel.Email;
+                objUser.FirstName = objUserModel.FirstName;
+                objUser.LastName = objUserModel.LastName;
+                objUser.Password = objUserModel.Password;
+
+                objUserDBEntities.Users.Add(objUser);
+                objUserDBEntities.SaveChanges();
+                objUserModel = new UserModel();
+                objUserModel.SuccessMessage = "User is Successfully Added";
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+        public ActionResult Login()
+        {
+            LoginModel objloginModel = new LoginModel();
+            return View(objloginModel);
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel objloginModel)
+        {
+            if (ModelState.IsValid)
+            {
+               if( objUserDBEntities.Users.Where(m => m.Email == objloginModel.Email && m.Password == objloginModel.Password).FirstOrDefault() == null)
+                {
+                    ModelState.AddModelError("Error", "Invalid Email and Password");
+                    return View();
+                }
+                else
+                {
+                    Session["Email"] = objloginModel.Email;
+                    return RedirectToAction("List", "Account");
+                }
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index","Home");
+        }
+        public ActionResult List()
+        {
+            var res = objUserDBEntities.Users.ToList();
+            return View(res);
+        }
+    }
+}
---
+++ UserModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppRegisLogin.Models
+{
+    public class UserModel
+    {
+        
+        public int UserId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "First Name is Required.")]
+        [Display(Name ="First Name:")]
+        public String FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name is Required.")]
+        [Display(Name = "Last Name:")]
+        public String LastName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is Required.")]
+        [Display(Name = "Email:")]
+        public String Email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is Required.")]
+        [Display(Name = "Password:")]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Confirm-Password is Required.")]
+        [Display(Name = "Confirm Password:")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password and confirm Password should be same")]
+        public String ConfirmPassword { get; set; }
+        
+        public DateTime CreatedOn { get; set; }
+        public string  SuccessMessage { get; set; }
+    }
+}
